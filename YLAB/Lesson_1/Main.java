1a6dbe251090692585dde972cae9af34aadaf218
Roman Sechenov <kpdprof@gmail.com>
2022-02-27 00:08:42 +0700

    Add files via upload

---
+++ YLAB/Lesson_1/Main.java
@@ -0,0 +1,29 @@
+package YLAB.Lesson_1;
+
+public class Main {
+    public static void main(String[] agrs) {
+        String name1 = "Hello1";
+        String name2 = "Hello2";
+        String name3 = "Hello3";
+        String s = "Когда я вырасту, то хочу быть паровым экскаватором!";
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+
+        System.out.println("\n");
+        System.out.println("Меня зовут Амиго.\n");
+        System.out.println("Я согласен на зарплату $800/месяц в первый год.");
+        System.out.println("Я согласен на зарплату $1500/месяц во второй год.");
+        System.out.println("Я согласен на зарплату $2200/месяц в третий год.");
+        System.out.println("Я согласен на зарплату $2700/месяц в четвертый год.");
+        System.out.println("Я согласен на зарплату $3200/месяц в пятый год.\n");
+        System.out.println("Поцелуй мой блестящий металлический зад!");
+    }
+}
